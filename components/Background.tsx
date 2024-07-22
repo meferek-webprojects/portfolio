70639424620bca3be2477438a27266b25c830f1d
Mateusz Krysiak <61255346+Meferek@users.noreply.github.com>
2024-07-22 14:14:15 +0200

    [BUGFIX]

--- components/Background.tsx
+++ components/Background.tsx
@@ -7,7 +7,7 @@ const Background = () => {
             <CircleLight color="darkblue" size="700px" position="-right-[0px] -bottom-[150px]"/> */}
             <div className={`animate-moveRight blur-3xl left-0 top-0 absolute w-[350px] h-[350px] bg-darkblue rounded-full`}></div>
             <div className={`animate-moveLeft blur-3xl right-[280px] -top-[450px] absolute w-[700px] h-[700px] bg-lightblue rounded-full`}></div>
-            <div className={`animate-moveRight blur-3xl -left-[80px] -bottom-[80px] absolute w-[460px] h-[450px] bg-lightblue rounded-full`}></div>
+            <div className={`animate-moveRight blur-3xl -left-[80px] -bottom-[80px] absolute w-[460px] h-[460px] bg-lightblue rounded-full`}></div>
             <div className={`animate-moveLeft blur-3xl -right-[0px] -bottom-[150px] absolute w-[700px] h-[700px] bg-darkblue rounded-full`}></div>
         </div>
     );
